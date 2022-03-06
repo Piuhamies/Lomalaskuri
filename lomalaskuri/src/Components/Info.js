d3b06135c10b9a2ae11fd9f5b66692579d40000c
Severi <severi.lybeck@gmail.com>
2022-03-06 17:27:43 +0200

    Infosivulle basic toiminnallisuus

--- lomalaskuri/src/Components/Info.js
+++ lomalaskuri/src/Components/Info.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export function Info(props) {
+    const [onSide, setOnSide] = useState("onSide1");
+    useEffect(() => {
+        let visibleFace = 0
+        const faceAmount = 2;
+        let rotateCube = (e) => {
+            console.log(e.deltaY);
+            if (e.deltaY > 0  && visibleFace + 1 <= faceAmount) {
+                visibleFace += 1;
+                console.log(visibleFace)
+                setOnSide(`onSide${visibleFace}`)
+            }
+            if (e.deltaY < 0 && visibleFace - 1 >= 0) {
+                visibleFace += -1;
+                console.log(visibleFace)
+                setOnSide(`onSide${visibleFace}`)
+            }
+        };
+        window.onwheel = rotateCube;
+    });
     return (
-       <div className="infoPage">
-           
-       </div>
+        <div className="infoPage">
+            <div className={`cube ${onSide}`}>
+                <div className={`cubeSide cubeSide1`}>
+                    <h1>L</h1>
+                    <h2>Lomalaskuri</h2>
+                    <p>2022</p>
+                </div>
+                <div className={`cubeSide cubeSide2`}>
+                    <h1>Lomalaskuri</h1>
+                    <p> Luotu</p>
+                </div>
+            </div>
+        </div>
     );
 }
--- lomalaskuri/src/Components/NewSchoolSelector.js
+++ lomalaskuri/src/Components/NewSchoolSelector.js
@@ -1,64 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
-export class NewSchoolSelector extends React.Component {
-  constructor() {
-    super();
-    this.toggle = this.toggle.bind(this);
+export function NewSchoolSelector(props) {
+  let history = useHistory();
+  const [additionalClass, setAdditionalClass] = useState(""); 
+  function toggle() {
+    props.toggleTheme(props.themes.login);
   }
-  toggle() {
-    this.props.toggleTheme(this.props.themes.login);
+  useEffect(() => {
+    props.toggleTheme(props.themes.login, true);
+  })
+  function changePage(url) {
+    setAdditionalClass("closing");
+    setTimeout(()=> {
+      history.push(url);
+    }, 500)
   }
-  componentDidMount() {
-    this.props.toggleTheme(this.props.themes.login, true);
-  }
-  render() {
-    return (
-      <>
-        <Helmet>
-          <link rel="preload" as="image" href="icons8-moon-symbol.svg" />{" "}
-          {/*joutuu uhrautumaan tekemään purkkavirityksiä jos meinaa saada lighthouse testistä hyvän tuloksen. */}
-        </Helmet>
-        <div className="loginSite">
-          <div className="lomaEsittely">
-            <div>
-              <div id="terveDiv">
-                <h1 id="terveText">Tervetuloa</h1>
-              </div>
-              <div id="lomaDiv">
-                <h1 id="lomaText">Lomalaskuriin!</h1>
-              </div>
+  return (
+    <>
+      <Helmet>
+        <link rel="preload" as="image" href="icons8-moon-symbol.svg" />{" "}
+        {/*joutuu uhrautumaan tekemään purkkavirityksiä jos meinaa saada lighthouse testistä hyvän tuloksen. */}
+      </Helmet>
+      <div className={`loginSite ${additionalClass}`}>
+        <div className="lomaEsittely">
+          <div>
+            <div id="terveDiv">
+              <h1 id="terveText">Tervetuloa</h1>
             </div>
-          </div>
-          <div className="login">
-            <h2> Valitse koulusi: </h2>
-            <div id="loginContainer">
-                <div className="loginBox">
-                  <div className="loginOptions">
-                  {this.props.schools.map((x, index) => (
-                    <Link
-                      onChange={() => console.log("change")}
-                      key={"kouluValinta" + index}
-                      className="schoolSelection"
-                      to={x.href + "/" + x.menuItems[0].nimi}
-                    >
-                      {x.schoolName}
-                    </Link>
-                  ))}
-                  </div>
-                  <Link id="infoLink" to={"/Info"}>Mikä on lomalaskuri?</Link>
-                </div>
-              <img
-                className="darkIcon"
-                alt="vaihda dark themeen"
-                onClick={this.toggle}
-                src="icons8-moon-symbol.svg"
-              />
+            <div id="lomaDiv">
+              <h1 id="lomaText">Lomalaskuriin!</h1>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="login">
+          <h2> Valitse koulusi: </h2>
+          <div id="loginContainer">
+            <div className="loginBox">
+              <div className="loginOptions">
+                {props.schools.map((x, index) => (
+                  <button
+                    key={"kouluValinta" + index}
+                    className="schoolSelection"
+                    onClick={() => {changePage(x.href + "/" + x.menuItems[0].nimi)} }
+                  >
+                    {x.schoolName}
+                  </button>
+                ))}
+              </div>
+              <button id="infoLink" onClick={() => {changePage("/info")}}>Mikä on Lomalaskuri?</button>
+            </div>
+            <img
+              className="darkIcon"
+              alt="vaihda dark themeen"
+              onClick={toggle}
+              src="icons8-moon-symbol.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

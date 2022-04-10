372fac780c4bba0acb6de81b9b7f7c9ec91d62b0
Severi Lybeck <severi.lybeck@gmail.com>
2022-04-10 19:07:57 +0300

    Tehty kuutiosta responsiivinen & runnattu prettier
    Vaihdettu prettier suosimaan tabulaattoreita. Tekee ainakin mielestäni lukemisesta helpompaa.

--- lomalaskuri/src/Components/Tilastot.js
+++ lomalaskuri/src/Components/Tilastot.js
@@ -1,32 +1,32 @@
 import React from "react";
 
 export default class Tilastot extends React.Component {
-  createStats() {
-    var negativeStats = () => {
-      return this.props.tiedot.negative.map((x, index) => (
-        <>
-          {" "}
-          <h2>{x}</h2>
-          <p className="result">Ei</p>
-        </>
-      ));
-    };
-    var positiveStats = () => {
-      return this.props.tiedot.positive.map((x, index) => (
-        <>
-          <h2>{x}</h2>
-          <p className="positiveResult">On</p>
-        </>
-      ));
-    };
-    return (
-      <>
-        {negativeStats()}
-        {positiveStats()}
-      </>
-    );
-  }
-  render() {
-    return <div id="tilastot">{this.createStats()}</div>;
-  }
+	createStats() {
+		var negativeStats = () => {
+			return this.props.tiedot.negative.map((x, index) => (
+				<>
+					{" "}
+					<h2>{x}</h2>
+					<p className="result">Ei</p>
+				</>
+			));
+		};
+		var positiveStats = () => {
+			return this.props.tiedot.positive.map((x, index) => (
+				<>
+					<h2>{x}</h2>
+					<p className="positiveResult">On</p>
+				</>
+			));
+		};
+		return (
+			<>
+				{negativeStats()}
+				{positiveStats()}
+			</>
+		);
+	}
+	render() {
+		return <div id="tilastot">{this.createStats()}</div>;
+	}
 }
--- lomalaskuri/src/Components/defaultMenu.js
+++ lomalaskuri/src/Components/defaultMenu.js
@@ -3,89 +3,89 @@ import Cookie from "js-cookie";
 import { Link, Redirect } from "react-router-dom";
 
 export class DefaultMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { curSchool: null, reset: false, prevReset: false };
-    this.changeSchool = this.changeSchool.bind(this);
-  }
-  componentDidMount() {
-    let curSchoolUrl = window.location.pathname.substring(
-      1,
-      window.location.pathname.indexOf("/", 1)
-    );
-    let curSchoolElem =
-      this.props.schools[
-        this.props.schools.findIndex((i) => i.href === curSchoolUrl)
-      ];
-    var properties =
-      curSchoolElem != null && curSchoolElem !== "undefined"
-        ? curSchoolElem.theme
-        : this.props.schools[0].theme;
-    this.props.updateDarkMode(properties, true);
-    if (this.state.reset === true && this.state.prevReset === false) {
-      this.setState({
-        curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
-        reset: true,
-        prevReset: true,
-      });
-    } else if (this.state.reset === true && this.state.prevReset === true) {
-      this.setState({
-        curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
-        reset: false,
-        prevReset: false,
-      });
-    } else {
-      this.setState({
-        curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
-      });
-    }
-  }
-  toggle = () => {
-    let darkMode = this.props.isDarkMode;
-    console.log(this.props.isDarkMode);
-    var properties =
-      this.state.curSchool.curSchoolElem != null &&
-      this.state.curSchool.curSchoolElem !== "undefined"
-        ? this.state.curSchool.curSchoolElem.theme
-        : this.props.schools[0].theme;
-    this.props.updateDarkMode(properties);
-  };
-  changeSchool() {
-    Cookie.set("site", null);
-    this.setState({ reset: true, prevReset: false });
-  }
-  render() {
-    let curSchool =
-      this.state.curSchool !== null
-        ? this.state.curSchool.curSchoolElem
-        : this.props.schools[0];
-    console.log(this.state.curSchool);
-    let menuItems = curSchool.menuItems.map((x) => (
-      <Link key={`menuItem${x.nimi}`} to={"/" + curSchool.href + "/" + x.nimi}>
-        {x.nimi}
-      </Link>
-    ));
-    console.log("render");
-    console.log(this.state);
-    return this.state.reset ? (
-      <Redirect to="/" />
-    ) : (
-      <>
-        {menuItems}
-        <button
-          className="linkLookALike"
-          onClick={this.toggle}
-          id="dynaaminenNappi">
-          Vaihda teemaa
-        </button>
-        <button className="linkLookALike" onClick={this.changeSchool}>
-          {" "}
-          Vaihda koulua
-        </button>
-        <Link to="/Info" className="linkLookALike">
-          Tietoa Lomalaskurista
-        </Link>
-      </>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { curSchool: null, reset: false, prevReset: false };
+		this.changeSchool = this.changeSchool.bind(this);
+	}
+	componentDidMount() {
+		let curSchoolUrl = window.location.pathname.substring(
+			1,
+			window.location.pathname.indexOf("/", 1)
+		);
+		let curSchoolElem =
+			this.props.schools[
+				this.props.schools.findIndex((i) => i.href === curSchoolUrl)
+			];
+		var properties =
+			curSchoolElem != null && curSchoolElem !== "undefined"
+				? curSchoolElem.theme
+				: this.props.schools[0].theme;
+		this.props.updateDarkMode(properties, true);
+		if (this.state.reset === true && this.state.prevReset === false) {
+			this.setState({
+				curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
+				reset: true,
+				prevReset: true,
+			});
+		} else if (this.state.reset === true && this.state.prevReset === true) {
+			this.setState({
+				curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
+				reset: false,
+				prevReset: false,
+			});
+		} else {
+			this.setState({
+				curSchool: { curSchoolUrl: curSchoolUrl, curSchoolElem: curSchoolElem },
+			});
+		}
+	}
+	toggle = () => {
+		let darkMode = this.props.isDarkMode;
+		console.log(this.props.isDarkMode);
+		var properties =
+			this.state.curSchool.curSchoolElem != null &&
+			this.state.curSchool.curSchoolElem !== "undefined"
+				? this.state.curSchool.curSchoolElem.theme
+				: this.props.schools[0].theme;
+		this.props.updateDarkMode(properties);
+	};
+	changeSchool() {
+		Cookie.set("site", null);
+		this.setState({ reset: true, prevReset: false });
+	}
+	render() {
+		let curSchool =
+			this.state.curSchool !== null
+				? this.state.curSchool.curSchoolElem
+				: this.props.schools[0];
+		console.log(this.state.curSchool);
+		let menuItems = curSchool.menuItems.map((x) => (
+			<Link key={`menuItem${x.nimi}`} to={"/" + curSchool.href + "/" + x.nimi}>
+				{x.nimi}
+			</Link>
+		));
+		console.log("render");
+		console.log(this.state);
+		return this.state.reset ? (
+			<Redirect to="/" />
+		) : (
+			<>
+				{menuItems}
+				<button
+					className="linkLookALike"
+					onClick={this.toggle}
+					id="dynaaminenNappi">
+					Vaihda teemaa
+				</button>
+				<button className="linkLookALike" onClick={this.changeSchool}>
+					{" "}
+					Vaihda koulua
+				</button>
+				<Link to="/Info" className="linkLookALike">
+					Tietoa Lomalaskurista
+				</Link>
+			</>
+		);
+	}
 }

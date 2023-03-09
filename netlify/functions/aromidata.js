3a018f84e85baac85f96f7b34287d88867535dde
Severi <severi.lybeck@gmail.com>
2023-03-09 20:34:41 +0200

    Refactored the backend

--- netlify/functions/aromidata.js
+++ netlify/functions/aromidata.js
@@ -1,58 +1,39 @@
-import request from request;
-import fetch from node-fetch;
+import fetch from "node-fetch";
 var aromiData = [
     { day: "Maanantai", food: "", veganFood: "" },
     { day: "Tiistai", food: "", veganFood: "" },
     { day: "Keskiviikko", food: "", veganFood: "" },
     { day: "Torstai", food: "", veganFood: "" },
     { day: "Perjantai", food: "", veganFood: "" },
-];
-exports.handler = async function (event, context) {
-    request({
-        headers: {
-            'origin': 'ruokalistat.espoocatering.fi', 'X-Requested-With': 'XMLHttpRequest'
-        },
-        uri: 'http://ruokalistat.espoocatering.fi/AromiStorageEspoo/blob/main/AromiMenusJsonData',
-        method: 'Get'
-    }, function (err, res, body) {
-        if (err) { return console.log(err); }
-        aromiJson = JSON.parse(body);
-        let intialData = aromiJson;
-        var date = new Date();
-        var dates = [];
-        var endDates = [];
-        for (var i in intialData.Restaurants) {
-            if (intialData.Restaurants[i].RestaurantId == "2856c0d5-d3f7-e511-bc65-60eb696ec4bb") {
-                var restaurantNumber = i;
-            }
+]
+    exports.handler = async function (event, context) {
+
+        //Find the menu url
+        const restaurantId = "2856c0d5-d3f7-e511-bc65-60eb696ec4bb";
+        const url = "http://ruokalistat.espoocatering.fi/AromiStorageEspoo/blob/main/AromiMenusJsonData";
+        const response = await fetch(url);
+        const data = await response.json();
+        const restaurantObj = data.Restaurants.find((obj) => obj.RestaurantId === restaurantId);
+        const curDate = new Date();
+        const menuObj = restaurantObj.JMenus.find((menu) => {
+            const startDate = new Date(menu.Start);
+            const endDate = new Date(menu.End);
+            return startDate < curDate && endDate > curDate;
+        });
+        const menuUrl = "https:" + menuObj.LinkUrl;
+
+
+        //Get the menu contents
+        const menuResponse = await fetch(menuUrl);
+        const menuData = await menuResponse.json();
+        menuData.Days.forEach((item, index) => {
+            aromiData[index].food = item.Meals[0].Name;
+            aromiData[index].veganFood = item.Meals[1].Name;
+        });
+
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(aromiData),
         };
-        for (var i in intialData.Restaurants[restaurantNumber].JMenus) {
-            dates.splice(i, 0, intialData.Restaurants[restaurantNumber].JMenus[i].Start);
-            endDates.splice(i, 0, intialData.Restaurants[restaurantNumber].JMenus[i].End);
-            var parsedDate = Date.parse(date);
-            if (parsedDate > Date.parse(dates[i]) && parsedDate < Date.parse(endDates[i])) {
-                var dateNumber = i;
-                menuFinalId = intialData.Restaurants[restaurantNumber].JMenus[dateNumber].MenuId;
-                request({
-                    headers: {
-                        'origin': 'ruokalistat.espoocatering.fi', 'X-Requested-With': 'XMLHttpRequest'
-                    },
-                    uri: `https://ruokalistat.espoocatering.fi/AromiStorageEspoo/blob/menu/${menuFinalId}`,
-                    method: 'Get'
-                }, function (err, res, body) {
-                    if (err) { return console.log(err); }
-                    let json = JSON.parse(body);
-                    json.Days.forEach((item, index) => {
-                        dayObject = json.Days["" + index];
-                        aromiData[index].food = dayObject.Meals[0].Name;
-                        aromiData[index].veganFood = dayObject.Meals[1].Name;
-                    });
-                });
-            }
-        }
-    });
-    return {
-        statusCode: 200,
-        body: JSON.stringify(aromiData),
-    };
-}
+    }
--- netlify/functions/otaniemiData.js
+++ netlify/functions/otaniemiData.js
@@ -1,5 +1,4 @@
-const request = require('request');
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 var otaniemiData = [
     { day: "Maanantai", food: "", veganFood: "" },
     { day: "Tiistai", food: "", veganFood: "" },
@@ -10,39 +9,41 @@ var otaniemiData = [
 exports.handler = async function (event, context) {
     const newDate = new Date();
     const currentDate = `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`
-    const url = `https://www.amica.fi/api/restaurant/menu/week?language=fi&restaurantPageId=330303&weekDate=${currentDate}`;
+    const url = `https://www.compass-group.fi/menuapi/week-menus?language=fi&costCenter=3868&date=${currentDate}`;
     const response = await fetch(url);
     const data = await response.json();
-    const weekMenu = data.LunchMenus;
-    for (const day in weekMenu) {
+    data.menus.forEach((daysMenu, day) => {
         try {
-            let menu = weekMenu[day].SetMenus[1].Meals;
-            let veganMenu = weekMenu[day].SetMenus[0].Meals;
-            let date = weekMenu[day].DayOfWeek;
+            console.log(daysMenu);
+            const menu = daysMenu.menuPackages[1].meals;
+            const veganMenu = daysMenu.menuPackages[0].meals;
+            const date = daysMenu.date;
 
             let food = []
-            for (item in menu) {
-                food.push(menu[item].Name);
-            }
-            food = food.join(" ja ");
+            menu.forEach((item, index) => {
+                food.push(item.name);
+                food.push(index === menu.length - 2 ? " ja " : ", ");
+            });
+            food.pop();
+            food = food.join("");
 
             let veganFood = []
-            for (item in veganMenu) {
-                veganFood.push(veganMenu[item].Name);
-            }
-            veganFood = veganFood.join(" ja ");
+            veganMenu.forEach((item, index) => {
+                veganFood.push(item.name);
+                veganFood.push(index === menu.length - 2 ? " ja " : ", ");
+            });
+            veganFood.pop();
+            veganFood = veganFood.join("");
 
-            otaniemiData[day].day = date;
             otaniemiData[day].food = food;
             otaniemiData[day].veganFood = veganFood;
         } catch (e) {
-            if (!(e instanceof TypeError)) {
-                console.error(e);
-            }
+            console.error(e);   
         }
-    }
+    });
     return {
         statusCode: 200,
-        body: otaniemiData,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(otaniemiData),
     };
 }
